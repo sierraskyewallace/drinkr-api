b0adb41c0baf01200801312098d078b3e357b4fa
sierraskyewallace <sskwallace@gmail.com>
2021-09-24 13:38:30 -0700

    added migrations and namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :drinks
+  
+  # The priority is based upon order of creation: first created -> highest priority.
+  namespace :api do
+    namespace :v1 do
+      resources :drinks, only: [:index, :create, :show, :update, :destroy]
+      resources :ingredients, only: [:index, :create, :show, :update, :destroy]
+      resources :instructions, only: [:index, :create, :show, :update, :destroy]
+      end 
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
