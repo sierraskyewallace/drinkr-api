945ea9557c99d44d3de9f2e5325192bae1fc017a
sierraskyewallace <sskwallace@gmail.com>
2021-09-24 15:26:14 -0700

    added index to ingredients and instructions

--- app/controllers/api/v1/instructions_controller.rb
+++ app/controllers/api/v1/instructions_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::InstructionsController < ApplicationController
+
+    def index
+        instructions = Instruction.all
+        render json: InstructionsSerializer.new(instructions)
+    end
 end
